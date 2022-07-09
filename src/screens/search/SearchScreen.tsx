57bd9d5aec1b3877f9dd41c34c0b0a65d42e143f
Ravindra Patle <me24tech@gmail.com>
2022-07-09 21:41:17 +0530

    Added search screen with search bar and list

--- src/screens/search/SearchScreen.tsx
+++ src/screens/search/SearchScreen.tsx
@@ -6,11 +6,46 @@ import {theme} from '../../constants/theme';
 const SearchScreen = () => {
   return (
     <Box f={1} bg="white">
-      <Text>Search Screen</Text>
+      <Box h={50} w="100%" px="sm" mt="sm" my="sm">
+        <TextInput
+          placeholder="search podcast"
+          style={styles.input}
+          selectionColor={theme.color.blueLight}
+        />
+      </Box>
+      <FlatList
+        style={styles.list}
+        data={[{id: 1}, {id: 2}, {id: 3}, {id: 4}]}
+        renderItem={() => (
+          <Box h={90} dir="row" align="center" px="sm">
+            <Box h={70} w={70} bg="blue" mr="sm" radius={10} />
+            <Box>
+              <Text bold>Ravindra Patle</Text>
+              <Text size="xs" color="grey">
+                This is sme title
+              </Text>
+              <Text size="xs" color="blueLight">
+                400 episode
+              </Text>
+            </Box>
+          </Box>
+        )}
+        keyExtractor={(item: any) => String(item.id)}
+      />
     </Box>
   );
 };
 
 export default SearchScreen;
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    flex: 1,
+    backgroundColor: theme.color.greyLighter,
+    borderRadius: 10,
+    paddingHorizontal: theme.space.sm,
+    fontSize: theme.text.size.md,
+  },
+  list: {minHeight: '100%'},
+});
